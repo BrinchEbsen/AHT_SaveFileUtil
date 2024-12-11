da2e7653c74a91a00333dbd970a9a3566c573fd8
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2024-12-11 21:32:05 +0100

    Add project files.

---
+++ AHT_SaveFileUtil/Save/SaveFile.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHT_SaveFileUtil.Save
+{
+    public class SaveFile
+    {
+        public GamePlatform Platform;
+
+        public uint CheckSum { get; }
+
+        private SaveFile() { }
+
+        public static SaveFile FromFileStream(FileStream stream, GamePlatform platform)
+        {
+            if (platform != GamePlatform.GameCube)
+            {
+                throw new NotImplementedException();
+            }
+
+            var file = new SaveFile();
+
+            // ...
+
+            return file;
+        }
+    }
+}
